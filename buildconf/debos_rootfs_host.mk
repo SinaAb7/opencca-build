0e732a4cb04fe6dde8613f7f7247d482ebd4b686
Andrin Bertschi <hi@abertschi.ch>
2025-08-06 20:23:13 +0200

    chore: quit debos build if download script fails

--- buildconf/debos_rootfs_host.mk
+++ buildconf/debos_rootfs_host.mk
@@ -16,22 +16,21 @@ MEMORY = 6Gb
 build: rk3588 ## build root file system
  
 $(OSPACK_IMAGE): $(OSPACK_YAML)
-	sudo chmod 777 /dev/kvm
-
+	sudo chmod 777 /dev/kvm || exit 1
 	cd $(DEBOS_DIR) && \
 	mkdir -p out && \
-    debos --cpus=$(CPUS) --memory=$(MEMORY) --artifactdir=out -t architecture:arm64 opencca-ospack-debian.yaml
+    debos --cpus=$(CPUS) --memory=$(MEMORY) --artifactdir=out -t architecture:arm64 opencca-ospack-debian.yaml || exit 1
 
 rk3588: $(OSPACK_IMAGE) download
-	sudo chmod 777 /dev/kvm
+	sudo chmod 777 /dev/kvm || exit 1
 	cd $(DEBOS_DIR) && \
 	debos --cpus=$(CPUS) --memory=$(MEMORY) --artifactdir=out -t architecture:arm64 \
-		-t platform:rock5b-rk3588 opencca-image-rockchip-rk3588.yaml
+		-t platform:rock5b-rk3588 opencca-image-rockchip-rk3588.yaml || exit 1
 
 # Delete PREBUILT_DIR to re-download
 $(PREBUILT_DIR)/.downloaded:
 	mkdir -p $(PREBUILT_DIR)
-	bash $(DOWNLOAD_PREBUILT)
+	bash $(DOWNLOAD_PREBUILT) || exit 1
 	touch $@
 
 .PHONY: download
